f4f7964fbc7844aa6b8eaa6cd8bf1eecf9d1bf75
Xia Jing <jingxyy@qq.com>
2018-09-23 15:42:31 +0800

    pass 909

--- src/909.cpp
+++ src/909.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 class Solution {
 public:
-    int valueAtI(vector<vector<int>>& board, int i) {
+        int valueAtI(vector<vector<int>>& board, int i) {
         int N = board.size();
         int rr = (i - 1) / N;
         int r = N - 1 - rr;
@@ -17,25 +17,65 @@ public:
     }
 
     int snakesAndLadders(vector<vector<int>>& board) {
-        
+        int N = board.size();
+        int target = N * N;
+        vector<int> min_steps(N*N+1, -1);
+        min_steps[1] = 0;
+        vector<int> pending, next_pending;
+        pending.push_back(1);
+        int steps = 0;
+        while(pending.size() > 0) {
+            steps ++;
+            for(int x : pending) {
+                for(int i = 0; i < 6; i++) {
+                    int next = x + i + 1;
+                    if (next == target) {
+                        return steps;
+                    }
+                    int board_value = valueAtI(board, next);
+                    if(min_steps[next] == -1 || (board_value != -1 && min_steps[board_value] == -1)) {
+                        min_steps[next] = steps;
+                        if(board_value != -1) {
+                            if (board_value == target) {
+                                return steps;
+                            }
+                            min_steps[board_value] = steps;
+                            next_pending.push_back(board_value);
+                        } else {
+                            next_pending.push_back(next);
+                        }
+                    }
+                }
+            }
+            swap(pending, next_pending);
+            next_pending.clear();
+        }
+        return -1;
     }
 };
 
 int main(){
+    int raw_board[8][8] = {
+        {-1, -1, -1, 46, 47, -1, -1, -1},
+        {51, -1, -1, 63, -1, 31, 21, -1},
+        {-1, -1, 26, -1, -1, 38, -1, -1},
+        {-1, -1, 11, -1, 14, 23, 56, 57},
+        {11, -1, -1, -1, 49, 36, -1, 48},
+        {-1, -1, -1, 33, 56, -1, 57, 21},
+        {-1, -1, -1, -1, -1, -1, 2, -1},
+        {-1, -1, -1, 8, 3, -1, 6, 56}};
+
     Solution s;
     vector<vector<int>> board;
-    int N = 4;
+    int N = 8;
     for(int i = 0; i < N; i ++){
         vector<int> row;
         for(int j = 0; j < N; j++) {
-            row.push_back(i*N+j);
+            row.push_back(raw_board[i][j]);
         }
         board.push_back(row);
     }
 
-    for(int i = 0; i < N*N; i++){
-        cout << s.valueAtI(board, i+1) << endl;
-    }
-
+    cout << s.snakesAndLadders(board) << endl;
     return 0;
 }
