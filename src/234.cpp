ad3c68f73e1bf3758cfe9a2eb6ead5d93efad094
Xia Jing <xia.jing@wanweifund.com>
2018-12-12 21:51:30 +0800

    pass 234 in two ways.

--- src/234.cpp
+++ src/234.cpp
@@ -11,6 +11,45 @@ using namespace std;
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
+        if(head == NULL || head->next == NULL){
+            return true;
+        }else if(head->next->next == NULL){
+            return head->val == head->next->val;
+        }
+
+        ListNode *slow = head, *quick = head->next;
+        ListNode *p1, *p2;
+        ListNode *backup = NULL;
+        while(1) {
+            if(quick->next == NULL) {
+                p1 = slow->next;
+                slow->next = backup;
+                p2 = slow;
+                break;
+            }else if(quick->next->next == NULL){
+                p1 = slow->next->next;
+                slow->next = backup;
+                p2 = slow;
+                break;
+            }
+            quick = quick->next->next;
+            ListNode* tmp = slow->next;
+            slow->next = backup;
+            backup = slow;
+            slow = tmp;
+        }
+
+        while(p1 != NULL){
+            if(p1->val != p2->val){
+                return false;
+            }
+            p1 = p1->next;
+            p2 = p2->next;
+        }
+        return true;
+    }
+
+    bool isPalindrome0(ListNode* head) {
         int l = 0;
         ListNode* p = head;
         while(p != NULL){
@@ -21,7 +60,7 @@ public:
             return true;
         }
 
-        int half = (l - 1) / 2;
+        int half = l / 2;
         int i = 0;
         ListNode* curr = NULL, *next = head;
         while(i < half) {
@@ -32,7 +71,7 @@ public:
             i ++;
         }
         if(l%2 == 1){
-            curr = curr->next;
+            next = next->next;
         }
 
         ListNode *p1 = curr, *p2 = next;
@@ -54,8 +93,13 @@ int main(){
 
     ListNode *head = new ListNode(1);
     ListNode *n1 = head->next = new ListNode(2);
-    ListNode *n2 = n1->next = new ListNode(1);
+    ListNode *n2 = n1->next = new ListNode(2);
     ListNode *n3 = n2->next = new ListNode(1);
+    cout << s.isPalindrome(head) << endl;
+
+    head = new ListNode(1);
+    n1 = head->next = new ListNode(0);
+    n2 = n1->next = new ListNode(0);
 
     cout << s.isPalindrome(head) << endl;
     return 0;
