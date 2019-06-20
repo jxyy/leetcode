b5eae5716efddcc41d9ec2916d88d5f795ddbf8a
Xia Jing <xia.jing@wanweifund.com>
2019-06-20 09:00:23 +0800

    pass 199

---
+++ gosrc/199/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func rdfs(node *TreeNode, depth int, res []int) []int {
+	fmt.Println(node.Val, depth, res)
+	if depth == len(res) {
+		res = append(res, node.Val)
+	}
+	if node.Right != nil {
+		res = rdfs(node.Right, depth+1, res)
+	}
+	if node.Left != nil {
+		res = rdfs(node.Left, depth+1, res)
+	}
+	return res
+}
+
+func rightSideView(root *TreeNode) []int {
+	if root == nil {
+		return []int{}
+	}
+	return rdfs(root, 0, []int{})
+}
+
+func main() {
+	var t = &TreeNode{1, nil, nil}
+	t.Left = &TreeNode{2, nil, nil}
+	t.Right = &TreeNode{3, nil, nil}
+	t.Left.Right = &TreeNode{5, nil, nil}
+	// t.Right.Right = &TreeNode{4, nil, nil}
+	fmt.Println(rightSideView(t))
+}
